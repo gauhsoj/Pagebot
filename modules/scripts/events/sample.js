239f00fdb9c4f62a4230823d03e250aeeac1d916
gauhsoj <coglatessodms@gmail.com>
2024-11-09 18:50:29 +0000

    asd

--- modules/scripts/events/sample.js
+++ modules/scripts/events/sample.js
@@ -2,20 +2,36 @@ module.exports.config = {
   name: "auto",
   author: "Deku",
   version: "1.0",
-  description:
-    "Griffith AI",
+  description: "Griffith AI",
   selfListen: false,
 };
+
 const axios = require("axios");
-module.exports.run = async function ({ event, args }) {
+
+module.exports.run = async function ({ event, api }) {
   function reply(q) {
     api.sendMessage(q, event.sender.id);
   }
+
   if (event.type === "message") {
     const base = "https://joshweb.click";
     const id = event.sender.id;
     const q = event.message.text;
-    const res = await axios.get(base + "/api/gpt-4o?q=" + q + "&uid=" + id);
-    reply(res.data.result);
+
+    try {
+      const res = await axios.get(`${base}/api/gpt-4o?q=${q}&uid=${id}`, {
+        timeout: 10000, 
+      });
+      reply(res.data.result);
+      console.log(res.data.result);
+    } catch (error) {
+      if (error.code === "ETIMEDOUT") {
+        console.error("Request timed out. Please try again later.");
+        reply("Sorry, the request timed out. Please try again later.");
+      } else {
+        console.error(error.message);
+        reply(error.message);
+      }
+    }
   }
 };
